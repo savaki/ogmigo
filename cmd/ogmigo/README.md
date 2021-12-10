cfff63cff725bb1343e28797301b2628b07150b9
Matt Ho <matt.ho@gmail.com>
2021-12-10 01:23:22 -0600

    added test harness to lib

---
+++ cmd/ogmigo/README.md
@@ -0,0 +1,4 @@
+ogmigo cli
+------------------------------------
+
+provides a simple test harness to validate ogmigo library.  no real other utility
---
+++ cmd/ogmigo/main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"regexp"
+	"strconv"
+	"sync/atomic"
+
+	"github.com/dgraph-io/badger/v3"
+	"github.com/savaki/ogmigo"
+	"github.com/savaki/ogmigo/ouroboros/chainsync"
+	"github.com/savaki/ogmigo/store/badgerstore"
+	"github.com/urfave/cli/v2"
+)
+
+var opts struct {
+	DB     string
+	Ogmios string
+	Points cli.StringSlice
+	Tick   int64
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:        "db",
+			Usage:       "db directory",
+			Value:       "testdb", // mainnet2
+			EnvVars:     []string{"DB_DIR"},
+			Destination: &opts.DB,
+		},
+		&cli.StringFlag{
+			Name:        "ogmios",
+			Usage:       "ogmios websocket endpoint",
+			Value:       "ws://3.21.245.207:1337", // mainnet2
+			EnvVars:     []string{"OGMIOS"},
+			Destination: &opts.Ogmios,
+		},
+		&cli.StringSliceFlag{
+			Name:        "point",
+			Aliases:     []string{"p"},
+			Usage:       "initial starting point in the form {slot}/{hash} e.g.",
+			EnvVars:     []string{"POINT"},
+			Destination: &opts.Points,
+		},
+		&cli.Int64Flag{
+			Name:        "tick",
+			Usage:       "display progress every tick slots",
+			EnvVars:     []string{"TICK"},
+			Value:       5e3,
+			Destination: &opts.Tick,
+		},
+	}
+	app.Action = action
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func action(_ *cli.Context) error {
+	db, err := badger.Open(badger.DefaultOptions(opts.DB))
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	client := ogmigo.New(
+		ogmigo.WithEndpoint(opts.Ogmios),
+		ogmigo.WithLogger(ogmigo.DefaultLogger),
+	)
+
+	var (
+		ctx    = context.Background()
+		re     = regexp.MustCompile(`^(\d+)/([a-zA-Z0-9]+)$`)
+		points chainsync.Points
+	)
+
+	for _, s := range opts.Points.Value() {
+		match := re.FindStringSubmatch(s)
+		if len(match) != 3 {
+			return fmt.Errorf("ogmigo: failed to parse point, %v", s)
+		}
+		slot, _ := strconv.ParseUint(match[1], 10, 64)
+		points = append(points, chainsync.PointStruct{
+			Hash: match[2],
+			Slot: slot,
+		}.Point())
+	}
+
+	var counter int64
+	var callback ogmigo.ChainSyncFunc = func(ctx context.Context, data []byte) error {
+		if v := atomic.AddInt64(&counter, 1); v%opts.Tick != 0 {
+			return nil
+		}
+
+		var response chainsync.Response
+		if err := json.Unmarshal(data, &response); err != nil {
+			return err
+		}
+		if response.Result == nil {
+			return nil
+		}
+		if response.Result.RollForward == nil {
+			return nil
+		}
+
+		ps := response.Result.RollForward.Block.PointStruct()
+		fmt.Printf("slot=%v hash=%v block=%v\n", ps.Slot, ps.Hash, ps.BlockNo)
+
+		return nil
+	}
+	closer, err := client.ChainSync(ctx, callback,
+		ogmigo.WithStore(badgerstore.New(db, "points")),
+		ogmigo.WithPoints(points...),
+	)
+
+	if err != nil {
+		return err
+	}
+	defer closer.Close()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Kill, os.Interrupt)
+
+	<-stop
+
+	return nil
+}
