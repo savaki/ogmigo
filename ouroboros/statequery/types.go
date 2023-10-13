d7b0f65361cebc54d90d40ccb526be4cb0415b06
rrruko <rukokarasu@gmail.com>
2023-10-13 13:11:12 -0700

    fix utxo Value

--- ouroboros/statequery/types.go
+++ ouroboros/statequery/types.go
@@ -2,9 +2,8 @@ package statequery
 
 import (
 	"encoding/json"
+	"fmt"
 	"math/big"
-
-	"github.com/SundaeSwap-finance/ogmigo/v6/ouroboros/chainsync"
 )
 
 type EraStart struct {
@@ -25,7 +24,7 @@ type Utxo struct {
 	Transaction UtxoTxID        `json:"transaction"`
 	Index       uint32          `json:"index"`
 	Address     string          `json:"address"`
-	Value       Ada             `json:"value"`
+	Value       Value           `json:"value"`
 	DatumHash   string          `json:"datumHash,omitempty"`
 	Datum       string          `json:"datum,omitempty"`
 	Script      json.RawMessage `json:"script,omitempty"`
@@ -35,6 +34,22 @@ type UtxoTxID struct {
 	ID string `json:"id"`
 }
 
-type Ada struct {
-	Ada chainsync.Lovelace `json:"ada"`
+type Value struct {
+	Ada    int64
+	Assets map[string]map[string]int64
+}
+
+func (v *Value) UnmarshalJSON(data []byte) error {
+	var m map[string]map[string]int64
+	if err := json.Unmarshal(data, &m); err != nil {
+		return err
+	}
+	adaAmt, ok := m["ada"]["lovelace"]
+	if !ok {
+		return fmt.Errorf("statequery: Value.UnmarshalJSON: key 'ada' missing")
+	}
+	delete(m, "ada")
+	v.Ada = adaAmt
+	v.Assets = m
+	return nil
 }
